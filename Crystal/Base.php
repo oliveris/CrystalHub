cfbf023af3d6b78a27efb8cdc4e682d3df58e9f0
Sam Oliveri <samoliveri92@gmail.com>
2018-04-19 12:03:47 +0100

    Package init commit

---
+++ Crystal/Base.php
@@ -0,0 +1,10 @@
+namespace Crystal;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Request;
+
+class Base
+{
+    const API_URL = "https://crystal.test/api/v1";
+}
